eed2c27c17043fbd3ba2b286911c942334f24bb1
Vugar Abdulali <vabdulaliyev@gmail.com>
2023-12-26 11:52:03 +0400

    auth

--- handlers/auth.go
+++ handlers/auth.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 	"net/http"
 
+	_ "github.com/23vugarr/content-sharing-app/cmd/docs"
+	"github.com/23vugarr/content-sharing-app/models"
 	"github.com/gin-gonic/gin"
 )
+
+type Handler struct {
+	DB *models.DB
+}
 
 func PostLogin(r *gin.Context) {
 	fmt.Println("inside post login")
@@ -15,4 +21,25 @@ func PostLogin(r *gin.Context) {
 func PostRegister(r *gin.Context) {
 	fmt.Println("inside post register")
 	r.IndentedJSON(http.StatusOK, gin.H{"message": "true"})
+}
+
+// GetUser       godoc
+// @Summary      Get books array
+// @Description  Responds with the list of all books as JSON.
+// @Tags         books
+// @Produce      json
+// @Success      200  {array}  models.Book
+// @Router       /books [get]
+func (h *Handler) GetUser(r *gin.Context) {
+	username := r.Param("username")
+	fmt.Println(username)
+
+	user, err := h.DB.FetchUserByUsername(username)
+	if err != nil {
+		r.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	r.JSON(http.StatusOK, gin.H{"user": user})
+
 }
--- models/user.go
+++ models/user.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 type User struct {
@@ -10,10 +11,25 @@ type User struct {
 }
 
 type UserScheme struct {
-	UserName string `json:"username"`
+	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
 func (user *User) PostUser() string {
 	return "test"
+}
+
+func (user *User) GetUser() string {
+	return "pass"
+}
+
+func (db *DB) FetchUserByUsername(username string) (UserScheme, error) {
+	var user User
+
+	err := db.DB.QueryRow("SELECT username, password FROM users WHERE username = $1", username).Scan(&user.User.Username, &user.User.Password)
+	if err != nil {
+		fmt.Println(err)
+		return UserScheme{}, err
+	}
+	return user.User, nil
 }
